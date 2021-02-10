21c4c21df903b7e61398a0fdc261c45799002925
azowyl <facugerstner_29@hotmail.com>
2021-02-10 11:09:20 -0300

    setup react native elements

--- src/HelloWorld.js
+++ src/HelloWorld.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { Text, View } from 'react-native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import {Icon} from 'react-native-elements';
 
 const HelloWorld = () => {
   return (
-    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Text>Hello World</Text>
-    </View>
+    <SafeAreaProvider>
+      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+        <Text>Hello World</Text>
+      </View>
+    </SafeAreaProvider>
   );
 };
 
