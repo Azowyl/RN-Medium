632b42c63a9ce834573dfef101167418f2b9f5cd
azowyl <facugerstner_29@hotmail.com>
2021-09-24 15:11:52 -0300

    move to hooks

--- src/Article/ArticleCreationForm/NewArticle.js
+++ src/Article/ArticleCreationForm/NewArticle.js
@@ -1,40 +1,32 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import NewArticleForm from './NewArticleForm';
-import {connect} from 'react-redux';
+import {connect, useDispatch} from 'react-redux';
 import {addNewArticle} from '../../Redux/actions/articles';
 
-class NewArticle extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {}
+const NewArticleHooks = ({ route, navigation }) => {
+  const [article, setArticle] = useState({});
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    setArticle({ ...route.params ? route.params.article : {} });
+  }, [route.params]);
+
+  const onChange = (input, value) => {
+    setArticle({...article, [input]: value});
   }
 
-  componentDidMount() {
-    this.setState({...this.props.route.params.article});
+  const onSubmit = () => {
+    dispatch(addNewArticle(article));
+    navigation.goBack();
   }
 
-  componentDidUpdate(prevProps, prevState) {
-    if (prevProps.route.params !== this.props.route.params) {
-      this.setState({...this.props.route.params.article});
-    }
-  }
-
-  onChange = (input, value) => {
-    this.setState({ [input]: value });
-  }
-
-  onSubmit = () => {
-    this.props.addNewArticle(this.state);
-    this.props.navigation.goBack();
-  }
-
-  render() {
-    return <NewArticleForm onChange={this.onChange} onSubmit={this.onSubmit} article={this.state} />
-  }
+  return (
+    <NewArticleForm
+      onChange={onChange}
+      onSubmit={onSubmit}
+      article={article}
+    />
+  );
 }
 
-const mapDispatchToProps = (dispatch) => ({
-  addNewArticle: (article) => dispatch(addNewArticle(article)),
-})
-
-export default connect(undefined, mapDispatchToProps)(NewArticle);
+export default NewArticleHooks;
